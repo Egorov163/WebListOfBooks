39a6b10b1e748bf2d55502d599c20c4f8adedfb3
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-10-08 12:21:30 +0400

    add db

---
+++ WebListOfBooks/DbStuff/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebListOfBooks.DbStuff.Models
+{
+    public class User : BaseModel
+    {
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }   
+        public string Email { get; set; }
+    }
+}
---
+++ WebListOfBooks/DbStuff/WebDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebListOfBooks.DbStuff.Models;
+
+namespace WebListOfBooks.DbStuff
+{
+    public class WebDbContext : DbContext
+    {
+        public WebDbContext(DbContextOptions options): base(options) { }    
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
